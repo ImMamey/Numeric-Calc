4f25768235521090d2ab522b6f4077a35d86c572
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-06-21 10:09:29 -0400

    Update simpson13.m

--- simpson13.m
+++ simpson13.m
@@ -19,3 +19,15 @@ xp=a+2*h:2*h:b-2*h
 xi=a+h:2*h:b-h
 f=@(x)sqrt(1+x.^2)
 I=(h/3)*(f(0)+4*sum(f(xi))+2*sum(f(xp))+f(2))
+
+
+#====================
+#si necesito mas dcimales
+format long
+# Otro ejemplo llamando a la funcion:
+I=simpson13(@(x)exp(-x.^2),0,1,10)
+#esto daria I=0.74682
+# Si quiero aumentar las cifras exactas, aumento el tamaño de N:
+I=simpson13(@(x)exp(-x.^2),0,1,100)
+I=simpson13(@(x)exp(-x.^2),0,1,10000)
+I=simpson13(@(x)exp(-x.^2),0,1,1000000)
